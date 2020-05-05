114c399b4957211ac6a614ff3c9ebdabc92141f6
shiki-sumiya <shiki.sumiya@weblio.co.jp>
2020-05-05 15:22:28 +0900

    I: add build item, and Todo

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart'; // TODO: 後でコメントアウトを外して変化を見る
+import 'package:flutter_app/todo.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // SystemChrome.setEnabledSystemUIOverlays([]); // TODO: 後でコメントアウトを外して変化を見る
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Hello eto',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -25,20 +30,27 @@ class TodoList extends StatefulWidget {
 }
 
 class _TodoListState extends State<TodoList> {
+  List<Todo> todos = [];
+  _toggleTodo(Todo todo, bool isChecked) {
+    todo.isDone = isChecked;
+  }
+  
+  Widget _buildItem(BuildContext context, int index) {
+    final todo = todos[index];
+    return CheckboxListTile(
+      value: todo.isDone,
+      title: Text(todo.title),
+      onChanged: (bool isChecked) {
+        _toggleTodo(todo, isChecked);
+      },
+    );
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      constraints: BoxConstraints.expand(
-        height: Theme.of(context).textTheme.display1.fontSize * 1.1 + 200.0,
-      ),
-      color: Colors.blue[600],
-      alignment: Alignment.center,
-      child: Text('Hello eto',
-        style: Theme.of(context)
-            .textTheme
-            .display1
-            .copyWith(color: Colors.white)),
-      transform: Matrix4.rotationZ(0.1),
+    return ListView.builder(
+      itemBuilder: _buildItem,
+      itemCount: todos.length,
     );
   }
 }
